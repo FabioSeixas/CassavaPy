b388eded0bac21eb10bb46cb509eb6fd3990e5e3
Fábio <flscosta94@gmail.com>
2020-05-05 01:39:44 -0300

    new documentation theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -47,7 +48,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,3 +55,10 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 master_doc = 'index'
+
+extensions = [
+    'sphinx_rtd_theme',
+]
+
+html_theme = "sphinx_rtd_theme"
+
