90daeb93c27661585dbb7450ddb7f937c2668fc3
FabioSeixas <flscosta94@gmail.com>
2020-10-29 01:27:45 -0300

    extract working for multi files

--- cassavapy/API/extract.py
+++ cassavapy/API/extract.py
@@ -9,9 +9,19 @@ def extract_experiment(json_file, tratmatrix):
         params = json.load(f)
 
     trat = len(tratmatrix)
-    years = 1
-    files = len(range(int(params["general"]["first_year"]), 
+
+    if params['general']['n_files'] == 'multi':
+        files = len(range(int(params["general"]["first_year"]), 
                       int(params["general"]["last_year"]) + 1))
+        years = 1
+    
+    elif params['general']['n_files'] == 'one':
+        files = [f'{params["general"]["file_name"]}{params["general"]["first_year"]}']
+        years = int(params["general"]["last_year"]) - int(params["general"]["first_year"]) + 1
+    
+    else:
+        raise ValueError(f"Value '{params['general']['n_files']}' not recognized for 'n_files' parameter.")
+
     modo = "Experimental"
 
     x = Output(trat=trat, years=years, mode=modo, files=files)
